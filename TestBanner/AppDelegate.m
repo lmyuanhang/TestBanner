df3f7ec165c58561416f0470f8be414cc998ea76
高海明 <gaohaiming@leilong.vip>
2018-11-14 15:33:04 +0800

    Revert "dev modfiy master push"
    This reverts commit 668941b52638effa39c1ce0bc3728aabbfae1e61.

--- TestBanner/AppDelegate.m
+++ TestBanner/AppDelegate.m
@@ -23,7 +23,7 @@
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
     
-    NSLog(@"TestDev");
+    NSLog(@"TestGit");
     
     ViewController *mainVC = [[ViewController alloc] init];
     
