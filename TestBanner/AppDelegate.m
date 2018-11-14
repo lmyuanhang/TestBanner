ce3f3d7e7dee6193ad627145fe36e8300c693a3c
高海明 <gaohaiming@leilong.vip>
2018-11-14 15:34:01 +0800

    test

--- TestBanner/AppDelegate.m
+++ TestBanner/AppDelegate.m
@@ -23,7 +23,7 @@
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
     
-    NSLog(@"TestGit");
+    NSLog(@"Test");
     
     ViewController *mainVC = [[ViewController alloc] init];
     
